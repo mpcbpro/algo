e46e2420428c0e53aacf1dc88e8bef235e68f915
mp <07___28@naver.com>
2022-03-30 23:59:32 +0900

    push

---
+++ 프로그래머스_기능개발.java
@@ -0,0 +1,27 @@
+package test;
+
+class 프로그래머스_기능개발 {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] temp = new int[100];
+        int day = -1;
+        for(int i = 0; i < progresses.length; i++){
+            while(progresses[i] + (speeds[i] * day) < 100){
+                day++;
+            }
+            temp[day]++;
+        }
+        int cnt = 0;
+        
+        for(int n : temp){
+            if(n != 0) cnt++;
+        }
+        
+        int[] answer = new int[cnt];
+        
+        cnt = 0;
+        for(int n : temp){
+            if(n != 0) answer[cnt++] = n;
+        }
+        return answer;
+    }
+}
